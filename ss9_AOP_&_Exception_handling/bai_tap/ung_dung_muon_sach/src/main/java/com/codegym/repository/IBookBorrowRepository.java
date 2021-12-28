b799d415039bf5433182c99d89ff9b8696489fb3
NguyenThanhDuong113bn <nguyenthanhduong113bn@gmail.com>
2021-12-28 17:24:10 +0700

    update

---
+++ ss9_AOP_&_Exception_handling/bai_tap/ung_dung_muon_sach/src/main/java/com/codegym/repository/IBookBorrowRepository.java
@@ -0,0 +1,8 @@
+package com.codegym.repository;
+
+
+import com.codegym.model.BookBorrow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IBookBorrowRepository extends JpaRepository<BookBorrow, Long> {
+}
